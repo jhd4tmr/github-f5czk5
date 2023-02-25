8915a3b6013aae5a9ca7acce419d332e2e0b90ad
jhd4tmr <jhd2215583@qq.com>
2023-02-25 22:15:50 +0800

    1 file modified, 2 added, 1 deleted

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    index
+    <br />
+    <a href="/p-p1-page">p-p1-page</a>
+    <br />
+    <a href="/p1-p2-page">p1-p2-page</a>
+    {{ data.test }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const { data } = await useFetch('/api/test');
+</script>
---
+++ server/api/test.ts
@@ -0,0 +1,5 @@
+export default defineEventHandler(async (event) => {
+  return {
+    test: 'a \n\n b \n\n c',
+  };
+});
