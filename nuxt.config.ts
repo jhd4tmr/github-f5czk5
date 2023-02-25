f17e4e89b8eddcf117789f350c415747f478d776
jhd4tmr <jhd2215583@qq.com>
2023-02-25 22:57:27 +0800

    1 file modified, 2 added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  hooks: {
+    'pages:extend'(pages) {
+      console.log(pages);
+    },
+  },
+});
---
+++ pages/parent-page.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>parent-page</div>
+</template>
